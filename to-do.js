325fb3b4c789d87ae2c7e6ce42748c4f1de715cc
Márjori Tamise <marjori.tamise@corebiz.com.br>
2020-04-07 20:22:02 -0300

    feat: criando variaveis com elementos necessarios para o projeto

---
+++ to-do.js
@@ -0,0 +1,9 @@
+let listElement = document.querySelector('#app ul'),
+    inputElement = document.querySelector('#app input'),
+    buttonElement = document.querySelector('#app button');
+
+let toDo = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+]
