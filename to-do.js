3359f1d7097df8a6e7b0506ca170e73519be238c
Márjori Tamise <marjori.tamise@corebiz.com.br>
2020-04-07 20:36:07 -0300

    feat: renderizando itens da ToDo com função

--- to-do.js
+++ to-do.js
@@ -2,8 +2,20 @@ let listElement = document.querySelector('#app ul'),
     inputElement = document.querySelector('#app input'),
     buttonElement = document.querySelector('#app button');
 
-let toDo = [
+let toDos = [
     'Fazer café',
     'Estudar JavaScript',
     'Acessar comunidade da Rocketseat'
 ];
+
+function renderToDos() {
+    for (toDo of toDos) {
+        let toDoElement = document.createElement('li'),
+            toDoText = document.createTextNode(toDo);
+
+        toDoElement.appendChild(toDoText);
+        listElement.appendChild(toDoElement)
+    }
+}
+
+renderToDos()
