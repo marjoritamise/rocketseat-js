2ba64b160f9536d66f12820950938e4db7f912b5
Márjori Tamise <marjori.tamise@corebiz.com.br>
2020-04-08 00:13:46 -0300

    feat: criando botão de exclusão da tarefa e atualizando o array

--- to-do.js
+++ to-do.js
@@ -11,12 +11,18 @@ var toDos = [
 function renderToDos() {
 
     listElement.innerHTML = '';
-    
+
     for (toDo of toDos) {
         var toDoElement = document.createElement('li'),
-            toDoText = document.createTextNode(toDo);
+            toDoText = document.createTextNode(toDo),
+            linkElement = document.createElement('a'),
+            linkText = document.createTextNode('Delet'),
+            position = toDos.indexOf(toDo);
 
-        toDoElement.appendChild(toDoText);
+        linkElement.appendChild(linkText);
+        toDoElement.append(toDoText, linkElement);
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', `deletToDo(${position})`);
         listElement.appendChild(toDoElement);
     }
 }
@@ -25,6 +31,11 @@ function addToDo() {
     let toDoText = inputElement.value;
     toDos.push(toDoText)
     inputElement.value = '';
+    renderToDos();
+}
+
+function deletToDo(position) {
+    toDos.splice(position, 1);
     renderToDos();
 }
 
